f741206190ff8c12a891245b902918072433d4e5
Kev Sturman <introvertsgo@gmail.com>
2022-06-13 20:00:59 +0100

    Add combobox from headless ui

--- components/places.tsx
+++ components/places.tsx
@@ -1,7 +1,10 @@
+import { Combobox } from "@headlessui/react";
+import { Fragment } from "react";
 import usePlacesAutocomplete, {
   getGeocode,
   getLatLng,
 } from "use-places-autocomplete";
+import { XCircleIcon } from "@heroicons/react/solid";
 //   import {
 //     Combobox,
 //     ComboboxInput,
@@ -37,27 +40,48 @@ export default function Places({ setOffice }: PlacesProps) {
     setOffice({ lat, lng });
   };
   return (
-    <div>
-      <input
-        type="text"
+    <div className="text-black">
+      <Combobox
         value={value}
-        onChange={(e) => setValue(e.target.value)}
-        style={{ marginBottom: "2em"}}
-      />
-      <div>
-        {status === "OK" &&
-          data.map(({ description }, index) => (
-            <div
-            style={{marginBottom: '1em', cursor: 'pointer'}}
-              key={index}
+        onChange={(e: string) => {
+          handleSelect(e);
+        }}
+      >
+        <div className="relative mt-1">
+          <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+            <Combobox.Input
+              className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+              onChange={(event) => setValue(event.target.value)}
+              displayValue={(value) => `${value}`}
+            />
+            <Combobox.Button
+              className="absolute inset-y-0 right-0 flex items-center pr-2"
               onClick={() => {
-                handleSelect(description);
+                setValue("");
               }}
             >
-              {description}
-            </div>
-          ))}
-      </div>
+              <XCircleIcon className="h-5 w-5 text-gray-400" />
+            </Combobox.Button>
+          </div>
+          <Combobox.Options>
+            {data.map(({ description }, index) => (
+              /* Use the `active` state to conditionally style the active option. */
+              /* Use the `selected` state to conditionally style the selected option. */
+              <Combobox.Option key={index} value={description} as={Fragment}>
+                {({ active, selected }) => (
+                  <li
+                    className={`${
+                      active ? "bg-blue-500 text-white" : "bg-white text-black"
+                    }`}
+                  >
+                    {description}
+                  </li>
+                )}
+              </Combobox.Option>
+            ))}
+          </Combobox.Options>
+        </div>
+      </Combobox>
     </div>
   );
 }
